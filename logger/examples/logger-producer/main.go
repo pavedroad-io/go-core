3b3a7874eeb93a02f717b904a29c1c8c45fd6a5b
Mark Green <mark@pavedroad.io>
2020-04-16 07:45:43 -0700

    Clean up example code, add Makefile for env example

--- logger/examples/logger-producer/main.go
+++ logger/examples/logger-producer/main.go
@@ -48,7 +48,7 @@ func main() {
 
 	log, err := logger.NewLogger(config)
 	if err != nil {
-		fmt.Printf("Could not instantiate zap logger %s", err.Error())
+		fmt.Printf("Could not instantiate zap logger: %s\n", err.Error())
 	} else {
 		log.Debugf("Zap using %s", "Debugf (should not appear)")
 		log.Infof("Zap using %s", "Infof")
@@ -68,7 +68,7 @@ func main() {
 	config.KafkaProducerCfg.Key = logger.LevelKey
 	log, err = logger.NewLogger(config)
 	if err != nil {
-		fmt.Printf("Could not instantiate logrus logger %s", err.Error())
+		fmt.Printf("Could not instantiate logrus logger: %s", err.Error())
 	} else {
 		log.Debugf("Logrus using %s", "Debugf (should not appear)")
 		log.Infof("Logrus using %s", "Infof")
@@ -86,7 +86,7 @@ func main() {
 	config.KafkaProducerCfg.KeyName = "subject"
 	log, err = logger.NewLogger(config)
 	if err != nil {
-		fmt.Printf("Could not instantiate logrus logger %s", err.Error())
+		fmt.Printf("Could not instantiate logrus logger: %s\n", err.Error())
 	} else {
 		log.Infof("Logrus using Infof and subject key (level)")
 		time.Sleep(time.Second)
@@ -97,7 +97,7 @@ func main() {
 	config.KafkaProducerCfg.Key = logger.TimeSecondKey
 	log, err = logger.NewLogger(config)
 	if err != nil {
-		fmt.Printf("Could not instantiate logrus logger %s", err.Error())
+		fmt.Printf("Could not instantiate logrus logger: %s\n", err.Error())
 	} else {
 		log.Infof("Logrus using Infof and seconds key")
 		time.Sleep(time.Second)
@@ -108,7 +108,7 @@ func main() {
 	config.KafkaProducerCfg.Key = logger.TimeNanoSecondKey
 	log, err = logger.NewLogger(config)
 	if err != nil {
-		fmt.Printf("Could not instantiate logrus logger %s", err.Error())
+		fmt.Printf("Could not instantiate logrus logger: %s\n", err.Error())
 	} else {
 		log.Infof("Logrus using Infof and nanoseconds key")
 		time.Sleep(time.Second)
