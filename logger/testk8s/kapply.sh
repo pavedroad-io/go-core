35f47276c849043f34b08edc474cd4d0ecfc3478
Mark Green <mark@pavedroad.io>
2022-03-18 04:19:22 -0700

    Clean up kafak k8s scripts

--- logger/testk8s/kapply.sh
+++ logger/testk8s/kapply.sh
@@ -2,8 +2,8 @@
 # sample script to apply and expose kafka resources
 kubectl apply -f zookeeper-deployment.yaml
 kubectl apply -f zookeeper-service.yaml
+kubectl wait --for condition=available deployment/zookeeper
 kubectl apply -f kafka-broker-deployment.yaml
 kubectl apply -f kafka-broker-service.yaml
-kubectl wait deployment/zookeeper --for condition=available
-kubectl wait deployment/kafka-broker --for condition=available
+kubectl wait --for condition=available deployment/kafka-broker
 kubectl expose deployment kafka-broker --name=my-broker --external-ip=192.168.64.2
--- logger/testk8s/kdelete.sh
+++ logger/testk8s/kdelete.sh
@@ -5,6 +5,8 @@ kubectl delete -f kafka-broker-deployment.yaml
 kubectl delete -f zookeeper-service.yaml
 kubectl delete -f zookeeper-deployment.yaml
 kubectl delete service my-broker
-kubectl wait --for=delete service/my-broker
-kubectl wait --for=delete deployment/Kafka-broker
+kubectl wait --for=delete service/kafka-service
+kubectl wait --for=delete deployment/kafka-broker
+kubectl wait --for=delete service/zookeeper
 kubectl wait --for=delete deployment/zookeeper
+kubectl wait --for=delete service/my-broker
