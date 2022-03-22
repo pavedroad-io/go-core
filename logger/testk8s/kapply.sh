cb2b20aba998348f76f26e2b5a35ee374d8149b4
Mark Green <mark@pavedroad.io>
2022-03-22 00:20:53 -0700

    Remove kafka service, exposing external IP sufficient

--- logger/testk8s/kapply.sh
+++ logger/testk8s/kapply.sh
@@ -4,6 +4,4 @@ kubectl apply -f zookeeper-deployment.yaml
 kubectl apply -f zookeeper-service.yaml
 kubectl wait --for condition=available deployment/zookeeper
 kubectl apply -f kafka-broker-deployment.yaml
-kubectl apply -f kafka-broker-service.yaml
-kubectl wait --for condition=available deployment/kafka-broker
 kubectl expose deployment kafka-broker --name=my-broker --external-ip=192.168.64.2
--- logger/testk8s/kdelete.sh
+++ logger/testk8s/kdelete.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # sample script to delete kafka resources
-kubectl delete -f kafka-broker-service.yaml
 kubectl delete -f kafka-broker-deployment.yaml
 kubectl delete -f zookeeper-service.yaml
 kubectl delete -f zookeeper-deployment.yaml
 kubectl delete service my-broker
-kubectl wait --for=delete service/kafka-service
 kubectl wait --for=delete deployment/kafka-broker
 kubectl wait --for=delete service/zookeeper
 kubectl wait --for=delete deployment/zookeeper
