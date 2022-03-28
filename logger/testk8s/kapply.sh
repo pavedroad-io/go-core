6e443246b29f6fec6347edbb392b5caf2fecbf3e
Mark Green <mark@pavedroad.io>
2022-03-27 20:49:32 -0700

    Use port forwarding in k8s kafka example config

--- logger/testk8s/kapply.sh
+++ logger/testk8s/kapply.sh
@@ -4,4 +4,6 @@ kubectl apply -f zookeeper-deployment.yaml
 kubectl apply -f zookeeper-service.yaml
 kubectl wait --for condition=available deployment/zookeeper
 kubectl apply -f kafka-broker-deployment.yaml
-kubectl expose deployment kafka-broker --name=my-broker --external-ip=192.168.64.2
+kubectl apply -f kafka-broker-service.yaml
+kubectl wait --for condition=available deployment/kafka-broker
+kubectl port-forward service/kafka-service 9092:9092
